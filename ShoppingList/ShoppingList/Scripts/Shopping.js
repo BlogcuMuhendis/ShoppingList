7d4fac23d9c85597737753959c1a0e4ddef6a1bd
Mert Metin <37337606+mertmtn@users.noreply.github.com>
2019-01-20 16:30:39 +0300

    Inıtial Commit
    Upload project files

---
+++ ShoppingList/ShoppingList/Scripts/Shopping.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module("shoppingListApp", []);
+app.controller("shoppingController", function ($scope) {
+
+    $scope.shoppingList = [];
+    $scope.shoppingList.itemName;
+    $scope.shoppingList.quantity; 
+
+    $scope.addList = function () {
+        $scope.shoppingList.push({ itemName: $scope.newItem, quantity: 1 });
+        $scope.shoppingList.quantity = 1;
+        $scope.newItem = '';
+    }
+
+    $scope.removeItem = function (index) {
+        $scope.shoppingList.splice(index, 1);
+    }
+
+    $scope.addOne = function (index) {
+        $scope.shoppingList[index].quantity += 1;
+    }
+
+    $scope.minusOne = function (index) {
+        $scope.shoppingList[index].quantity -= 1;
+        if ($scope.shoppingList[index].quantity == 0) {
+            $scope.shoppingList.splice(index, 1);
+        }
+    }
+
+});
